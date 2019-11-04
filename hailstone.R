d4a86a79ef33e93d7cf5be058bc054125e24882e
RileyPBall <54775361+RileyPBall@users.noreply.github.com>
2019-11-03 21:15:13 -0800

    Add files via upload

---
+++ hailstone.R
@@ -0,0 +1,14 @@
+counter <- 0
+seqlen <- seq()
+for (i in 1:1000){
+  n <- i
+  while (n != 1) {
+    cat(n,"\n")
+    if (n%%2==0) {
+      n<-as.integer(n/2)
+  } else {
+      n<-as.integer(3*n+1)
+  }
+    counter <- counter+1
+  }
+}
